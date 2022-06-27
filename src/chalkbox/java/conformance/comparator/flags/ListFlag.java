8b77a5104b8464f392dd0fe543882c414c9e6b8e
caleb-wishart <caleb.wish@outlook.com>
2022-06-27 10:36:54 +1000

    Update to fix toString of ListFlag
    Now shows the correct error messages.
    The same HashSet technique could be used in the `isSet` method but the performance gain is debatable

--- src/chalkbox/java/conformance/comparator/flags/ListFlag.java
+++ src/chalkbox/java/conformance/comparator/flags/ListFlag.java
@@ -2,6 +2,7 @@ package chalkbox.java.conformance.comparator.flags;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class ListFlag<T> extends Flag {
@@ -42,16 +43,25 @@ public class ListFlag<T> extends Flag {
         List<T> missing = new ArrayList<>();
         List<T> extra = new ArrayList<>();
 
-        for (T expect : expected) {
-            if (!actual.contains(expect)) {
-                missing.add(expect);
+        for (T expect : new HashSet<>(expected)) {
+            int freqExpected = Collections.frequency(expected,expect);
+            int freqActual = Collections.frequency(actual, expect);
+            if(freqExpected != freqActual) {
+                for (int i = 0; i < Math.max(0, freqExpected); i++) {
+                    missing.add(expect);
+                }
             }
         }
 
-        for (T actual : actual) {
-            if (!expected.contains(actual)) {
-                extra.add(actual);
+        for (T act : new HashSet<>(actual)) {
+            int freqExpected = Collections.frequency(expected,act);
+            int freqActual = Collections.frequency(actual, act);
+            if(freqExpected != freqActual) {
+                for(int i = 0; i < freqActual; i++) {
+                    extra.add(act);
+                }
             }
+
         }
 
         builder.append(message)
